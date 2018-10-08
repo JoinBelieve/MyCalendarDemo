d5d577228c635a0925ca2164ef034f6b15bc1302
gituser <root@ommoo.net>
2018-10-08 16:37:48 +0800

    save

--- app/src/main/java/com/dan/mycalendardemo/MainActivity.kt
+++ app/src/main/java/com/dan/mycalendardemo/MainActivity.kt
@@ -15,7 +15,8 @@ import com.haibin.calendarview.CalendarView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
-import java.util.HashMap
+import java.util.*
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity(), CalendarView.OnCalendarInterceptListener
         , CalendarView.OnCalendarRangeSelectListener {
@@ -26,6 +27,8 @@ class MainActivity : AppCompatActivity(), CalendarView.OnCalendarInterceptListen
     private var orderSummart: OrderSummart? = null
 
     private var start: Calendar? = null
+
+    private var orderSumMap: HashMap<String, Any>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,7 +53,13 @@ class MainActivity : AppCompatActivity(), CalendarView.OnCalendarInterceptListen
                 , Special("20181011", 2000), Special("20181012", 2500)))
 
         //预定信息
-        orderSummart = OrderSummart(mutableListOf("20180930", "20181007", "20181009"))
+        orderSummart = OrderSummart(mutableListOf("20181030", "20181025", "20181024"))
+
+        orderSumMap = hashMapOf()
+        orderSummart!!.orderSummary.forEach {
+            orderSumMap!![it] = "已租"
+        }
+
 
         val map = HashMap<String, Calendar>()
         val maxYear = mCalendarView.maxRangeCalendar.year
@@ -97,41 +106,41 @@ class MainActivity : AppCompatActivity(), CalendarView.OnCalendarInterceptListen
 
     override fun onCalendarIntercept(calendar: Calendar): Boolean {
         //判断是否在日期范围之内
-        if (DateUtil.isCalendarInRange(calendar, mCalendarView.minRangeCalendar.year,
-                        mCalendarView.minRangeCalendar.month, mCalendarView.minRangeCalendar.day,
-                        mCalendarView.maxRangeCalendar.year, mCalendarView.maxRangeCalendar.month, mCalendarView.maxRangeCalendar.day)) {
-            val year = calendar.year
-            val month = calendar.month
-            val day = calendar.day
-//        mCalendarView.minRangeCalendar
-            if (start == null) {
-                return orderSummart?.orderSummary?.any {
-                    year == it.substring(0..3).toInt() &&
-                            month == it.substring(4..5).toInt() &&
-                            day == it.substring(6..7).toInt()
-                } ?: false
-            } else {
-                val sYear = start!!.year
-                val sMonth = start!!.month
-                val sDay = start!!.day
-                val startCalendar = java.util.Calendar.getInstance()
-                startCalendar.set(java.util.Calendar.YEAR, sYear)
-                startCalendar.set(java.util.Calendar.MONTH, sMonth)
-                startCalendar.set(java.util.Calendar.DAY_OF_MONTH, sDay)
-
-                val allCalendar = java.util.Calendar.getInstance()
-                allCalendar.set(java.util.Calendar.YEAR, year)
-                allCalendar.set(java.util.Calendar.MONTH, month)
-                allCalendar.set(java.util.Calendar.DAY_OF_MONTH, day)
-
-                if (startCalendar.timeInMillis > allCalendar.timeInMillis) {
-                    Log.e("TAG", "比入住之前的日期 $year-$month-$day")
-                } else if (startCalendar.timeInMillis < allCalendar.timeInMillis) {
-                    Log.e("TAG", "比入住之后的日期 $year-$month-$day")
-                }
-            }
-        }
-        return false
+//        if (DateUtil.isCalendarInRange(calendar, mCalendarView.minRangeCalendar.year,
+//                        mCalendarView.minRangeCalendar.month, mCalendarView.minRangeCalendar.day,
+//                        mCalendarView.maxRangeCalendar.year, mCalendarView.maxRangeCalendar.month, mCalendarView.maxRangeCalendar.day)) {
+//            val year = calendar.year
+//            val month = calendar.month
+//            val day = calendar.day
+////        mCalendarView.minRangeCalendar
+//            if (start == null) {
+//                return orderSummart?.orderSummary?.any {
+//                    year == it.substring(0..3).toInt() &&
+//                            month == it.substring(4..5).toInt() &&
+//                            day == it.substring(6..7).toInt()
+//                } ?: false
+//            } else {
+//                val sYear = start!!.year
+//                val sMonth = start!!.month
+//                val sDay = start!!.day
+//                val startCalendar = java.util.Calendar.getInstance()
+//                startCalendar.set(java.util.Calendar.YEAR, sYear)
+//                startCalendar.set(java.util.Calendar.MONTH, sMonth)
+//                startCalendar.set(java.util.Calendar.DAY_OF_MONTH, sDay)
+//
+//                val allCalendar = java.util.Calendar.getInstance()
+//                allCalendar.set(java.util.Calendar.YEAR, year)
+//                allCalendar.set(java.util.Calendar.MONTH, month)
+//                allCalendar.set(java.util.Calendar.DAY_OF_MONTH, day)
+//
+//                if (startCalendar.timeInMillis > allCalendar.timeInMillis) {
+//                    Log.e("TAG", "比入住之前的日期 $year-$month-$day")
+//                } else if (startCalendar.timeInMillis < allCalendar.timeInMillis) {
+//                    Log.e("TAG", "比入住之后的日期 $year-$month-$day")
+//                }
+//            }
+//        }
+        return orderSumMap == null || orderSumMap?.size == 0 || orderSumMap?.containsKey(calendar.toString())!!
     }
 
     override fun onCalendarSelectOutOfRange(calendar: Calendar) {
